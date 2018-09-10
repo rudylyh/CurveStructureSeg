cc9d244807dc525c9631ce46426e1a987af81562
Yuhang Lu <rudylyh@gmail.com>
2018-09-10 16:27:13 -0400

    Update depth2curve.py

--- depth2curve.py
+++ depth2curve.py
@@ -2,7 +2,7 @@ import cv2
 import caffe
 import numpy
 from skimage.morphology import skeletonize
-caffe.set_mode_cpu()
+caffe.set_mode_gpu()
 
 def Normalize(image):
     min_val = min(map(min, image))
